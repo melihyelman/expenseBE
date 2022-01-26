d1e998ba2871047997f4f42a3a4098d61bf13e66
melihyelman <myelman17@gmail.com>
2022-01-26 23:36:03 +0300

    add validations and validate middleware

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { index, create } = require('../controllers/Users');
+const validate = require('../middlewares/validate');
+const validations = require('../validations/Users');
 
 const router = express.Router();
 
 router.get("/", index)
-router.post("/", create)
+router.post("/", validate(validations.createValidation), create)
 
 module.exports = router;
---
+++ src/validations/Users.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+
+const createValidation = Joi.object({
+    full_name: Joi.string().required().min(5).max(50),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(5).max(20),
+})
+
+module.exports = {
+    createValidation
+}
