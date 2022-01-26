281538f4faa1c619f82466e8dc4adabdf2d11324
melihyelman <myelman17@gmail.com>
2022-01-27 01:26:43 +0300

    add authenticate

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,18 @@
+const httpStatus = require('http-status');
+const JWT = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.headers?.authorization?.split(" ")[1] || null;
+
+    if (!token)
+        return res.status(httpStatus.UNAUTHORIZED).send({ error: "Bu işlemi yapmak için giriş yapmalısınız..." });
+
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, decoded) => {
+        if (err)
+            return res.status(httpStatus.FORBIDDEN).send({ error: "Token süresi geçmiştir..." });
+        res.user = decoded;
+        next();
+    })
+}
+
+module.exports = authenticate;
