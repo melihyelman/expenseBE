e80ad32cddc8d11e545691800f2e10d8202cfce1
melihyelman <myelman17@gmail.com>
2022-01-29 10:10:05 +0300

    review db connection

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -9,7 +9,7 @@ db.once("open", () => {
 
 
 const connectDB = async () => {
-    await Mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+    await Mongoose.connect(`mongodb+srv://${process.env.DB_HOST}${process.env.DB_PORT}/${process.env.DB_NAME}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
